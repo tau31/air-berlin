2697a38d57150dfe2e97f7de866abaf17ef3d0ce
tau31 <tau31@pm.me>
2025-03-18 10:03:58 +0100

    improved order of categories (helpfuld for vizualizations)

--- fetch.R
+++ fetch.R
@@ -10,7 +10,6 @@ berlin_loc <- request("http://api.openweathermap.org/geo/1.0/direct?") |>
   as.data.frame() |>
   dplyr::select(lat, lon)
 
-# q=Berlin,,DE&limit=1&appid=4068918a8b685d4fe4a3ec34ec0e5c2b
 
 air_data_resp <- request("http://api.openweathermap.org/data/2.5/air_pollution/history") |>
   req_url_query(
@@ -48,13 +47,26 @@ air_pm[, `:=`(
 # Add WHO recommendation
 # https://www.who.int/publications/i/item/9789240034228
 
+openWeather_guide <- c("Good", "Fair", "Moderate", "Poor_or_Very_Poor")
+who_guideline <- c("meets_guideline", "2_times_higher", "3_times_higher", "5_times_higher", "7_times_higher", "10_times_higher", "over_10_times_higher")
+
+
 rec <- data.table(
-  qualitative = c("Good", "Good", "Fair", "Fair", "Moderate", "Moderate", "Poor_or_Very_Poor"),
-  who_guideline = c("meets_guideline", "2_times_higher", "3_times_higher", "5_times_higher", "7_times_higher", "10_times_higher", "over_10_times_higher"),
+  qualitative = factor(
+    c("Good", "Good", "Fair", "Fair", "Moderate", "Moderate", "Poor_or_Very_Poor"),
+    ordered = TRUE,
+    levels = openWeather_guideline
+  ),
+  who_guideline = factor(
+    who_guideline,
+    ordered = TRUE,
+    levels = who_guideline
+  ),
   min = c(0, 5, 10, 15, 25, 35, 50),
   max = c(5, 10, 15, 25, 35, 50, Inf)
 )
 
+# non-equi join
 air_pm[rec, `:=`(qualitative = i.qualitative, who_guideline = i.who_guideline), on = .(value >= min, value < max)]
 
 saveRDS(air_pm, "data/air_pm.rds")
